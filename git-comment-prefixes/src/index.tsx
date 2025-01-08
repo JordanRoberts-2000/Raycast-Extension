17b7093c4d856ebbfb6692c416186fd836aa17cd
Jordan Roberts <jordanroberts333@icloud.com>
2025-01-08 12:10:58 +0000

    feat(prefix) - added common prefix list

--- git-comment-prefixes/src/index.tsx
+++ git-comment-prefixes/src/index.tsx
@@ -3,7 +3,23 @@ import { List } from "@raycast/api";
 export default function Command() {
   return (
     <List>
-      <List.Item title={"Feat"} subtitle={"Introduces a new feature"} />
+      <List.Item title={"Feat:     "} subtitle={"Introduces a new feature."} />
+      <List.Item title={"Fix:      "} subtitle={"Fixes a bug."} />
+      <List.Item title={"Refactor: "} subtitle={"Refactors code without changing behavior."} />
+      <List.Item title={"Perf:     "} subtitle={"Improves performance."} />
+      <List.Item
+        title={"Style:    "}
+        subtitle={"Changes related to code style or formatting (does not affect behavior)."}
+      />
+      <List.Item title={"Build:    "} subtitle={"Changes that affect the build system or dependencies."} />
+      <List.Item
+        title={"Chor:     "}
+        subtitle={"Miscellaneous tasks, like configuration changes or non-functional updates."}
+      />
+      <List.Item title={"Docs:     "} subtitle={"Documentation updates."} />
+      <List.Item title={"Test:     "} subtitle={"Adding or updating tests."} />
+      <List.Item title={"Cli:      "} subtitle={"Changes related to continuous integration or deployment."} />
+      <List.Item title={"Revert:   "} subtitle={"Reverts a previous commit."} />
     </List>
   );
 }
